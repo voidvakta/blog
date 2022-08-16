f0832d8db0f9e472819c5474449e4b53c1b7bd66
Siddharth Movaliya <siddraj1992@gmail.com>
2022-08-16 16:42:09 +0530

    Updated Favicon

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from flask_gravatar import Gravatar
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY", "8BYkEfBA6O6donzWlSihBXox7C0sKR6b")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 login_manager = LoginManager(app)
